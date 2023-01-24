d27ae9da39ea6bf0553c29f325f3e3bcbeb2b110
Mumuni Tahiru <peptahiru@gmail.com>
2023-01-24 21:33:29 +0000

    commit

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,15 @@
-
+pcChoice=function getComputerChoice(){
+    let choice;
+    let randNum=Math.floor(Math.random()*3)+1;
+    switch(randNum){
+        case 1:
+            choice="rock";
+            break;
+        case 2:
+            choice="paper";
+        case 3:
+            choice="scissors";
+            }
+    return choice;
+}
+console.log(pcChoice());
